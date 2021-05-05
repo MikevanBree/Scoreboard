aebd50e3aae20b919f9d90e1897947f14d3e0f22
IkBenWolf Games <ikbenwolfgames@gmail.com>
2021-05-05 09:29:39 +0200

    added arrow support for scores and round limit

--- opgave62.py
+++ opgave62.py
@@ -3,10 +3,16 @@ import pygame
 WIDTH = 1000
 HEIGHT = 600
 
+teamscore1 = 0
+teamscore2 = 0
+teamgame1 = 0
+teamgame2 = 0
+set_score = 5
+
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-
+# fonts
 fonttext = pygame.font.SysFont("opensans", 80)
 font = pygame.font.SysFont("opensans", 100)
 fontsmall = pygame.font.SysFont("opensans", 65)
@@ -16,30 +22,47 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                teamscore1 = 0
+                teamscore2 = 0
+                teamgame1 = 0
+                teamgame2 = 0
+                set_score = 5
+            elif event.key == pygame.K_RIGHT:
+                teamscore1 = teamscore1 + 1
+            elif event.key == pygame.K_LEFT:
+                teamscore2 = teamscore2 + 1
+    
+    if teamscore1 >= set_score:
+        teamscore1 = 0
+        teamgame1 = teamgame1 + 1
+    if teamscore2 >= set_score:
+        teamscore2 = 0
+        teamgame2 = teamgame2 + 1
 
     screen.fill("black")
 
+    # definitions Teams
     teamname1 = "Ospel"
     teamname2 = "Maaseik"
-    teamscore1 = str(0)
-    teamscore2 = str(0)
-    teamgame1 = str(0)
-    teamgame2 = str(0)
     
+    # Teamnames render
     team1 = fonttext.render(teamname1, True, "#EB5E0C")
     screen.blit(team1, (75, 300))
     team2 = fonttext.render(teamname2, True, "#EB5E0C")
     screen.blit(team2, (690, 300))
 
-
-    teamnumber1 = font.render(teamscore1, True, "white")
+    # Teamrounds render
+    teamnumber1 = font.render(str(teamscore1), True, "white")
     screen.blit(teamnumber1, (150, 450))
-    teamnumber2 = font.render(teamscore2, True, "white")
+    teamnumber2 = font.render(str(teamscore2), True, "white")
     screen.blit(teamnumber2, (800, 450))
 
-    teamset1 = fontsmall.render(teamgame1, True, "white")
+    #Team sets render
+    teamset1 = fontsmall.render(str(teamgame1), True, "white")
     screen.blit(teamset1, (250, 450))
-    teamset2 = fontsmall.render(teamgame2, True, "white")
+    teamset2 = fontsmall.render(str(teamgame2), True, "white")
     screen.blit(teamset2, (700, 450))
 
     pygame.display.flip()
