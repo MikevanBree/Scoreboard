b7a651c3783bc0a2164a82412ca0ff7f2ea66d67
IkBenWolf Games <ikbenwolfgames@gmail.com>
2021-06-02 10:02:03 +0200

    Lorem Ipsum

--- QtWindow.py
+++ QtWindow.py
@@ -4,9 +4,20 @@ from PySide6 import QtWidgets
 config = configparser.ConfigParser()
 config.read("config.ini")
 
+logofile1 = config["TeamA"]["logo"]
+logofile2 = config["TeamB"]["logo"]
+def openLogo1():
+    logofile1 = QtWidgets.QFileDialog.getOpenFileName()[0]
+
+def openLogo2():
+    logofile2 = QtWidgets.QFileDialog.getOpenFileName()[0]
+
+
 def Save():
     config["TeamA"]["naam"] = editname1.toPlainText()
     config["TeamB"]["naam"] = editname2.toPlainText()
+    config["TeamA"]["logo"] = logofile1
+    config["TeamB"]["logo"] = logofile2
     configfile = open("./config.ini", "wt")
     config.write(configfile)
     configfile.close()
@@ -23,9 +34,19 @@ label1 = QtWidgets.QLabel("Team 1:")
 editname1 = QtWidgets.QTextEdit(config["TeamA"]["naam"])
 layout.addRow(label1, editname1)
 
+labellogo1 = QtWidgets.QLabel("Logo Team 1:")
+logo1 = QtWidgets.QPushButton("Browse")
+logo1.clicked.connect(openLogo1)
+layout.addRow(labellogo1, logo1)
+
 label2 = QtWidgets.QLabel("Team 2:")
 editname2 = QtWidgets.QTextEdit(config["TeamB"]["naam"])
 layout.addRow(label2, editname2)
+
+labellogo2 = QtWidgets.QLabel("Logo Team 2:")
+logo2 = QtWidgets.QPushButton("Browse")
+logo2.clicked.connect(openLogo2)
+layout.addRow(labellogo2, logo2)
 
 button = QtWidgets.QPushButton("Save")
 layout.addRow(button)
